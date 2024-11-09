905ed5b753c66e7a09307b23018a17646ea5fd87
wijsmane <wijsmane@tcd.ie>
2024-11-09 13:57:30 +0000

    first commit

---
+++ src/main/java/ie/tcd/scss/group16/Group16Application.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.group16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group16Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/tcd/scss/group16/Group16ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.group16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
